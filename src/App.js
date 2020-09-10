c62c88b6b5c3d95b66da7f3d40bcf28d1f781e08
James Nowecki <james.nowecki@minstersoft.co.uk>
2020-09-10 13:10:10 +0100

    basics complete

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import Button from "./components/Button"
 import LogoutButton from './components/LogoutButton/LogoutButton';
 import Profile from "./components/Profile"
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+  if (isLoading) return <div>Loading your data</div>
   return (
     <>
     <Button />
