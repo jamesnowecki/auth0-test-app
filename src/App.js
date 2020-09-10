447093d3b4d84d15974a30a75bb253af1d96cc5f
James Nowecki <james.nowecki@minstersoft.co.uk>
2020-09-10 10:45:27 +0100

    logout fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Button from "./components/Button"
+import LogoutButton from './components/LogoutButton/LogoutButton';
 
 function App() {
   return (
+    <>
     <Button />
+    <LogoutButton />
+    </>
   );
 }
 
